fd20ddf5da82d981b615c3dd0024a1886d9d16d0
SarahIrons <sarahirons11@gmail.com>
2024-01-30 15:24:12 -0500

    Add project files.

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringAssignment
+{
+    class Program
+    {//STRING ASSIGNMENT
+     //Create a console app that does/includes the following:
+     //Concatenates three strings.
+     //Converts a string to uppercase.
+     //Creates a Stringbuilder that builds a paragraph of text, one sentence at a time.
+        static void Main(string[] args)
+        {
+            string a = "I have ";
+            string b = "two beautful, perfect ";
+            string c = "feline housemates.";
+            Console.WriteLine(a + b + c);
+
+            string cats = "Jody + Carlo furrever!";
+            Console.WriteLine(cats.ToUpper());
+
+            StringBuilder x = new StringBuilder("So much depends upon ");
+            x.AppendLine("a red wheelbarrow ");
+            x.AppendLine("glazed with rain water ");
+            x.AppendLine("beside the white chickens ");
+            x.AppendLine("-William Carlos Williams");
+            Console.WriteLine(x);
+                   
+        }
+    }
+}
